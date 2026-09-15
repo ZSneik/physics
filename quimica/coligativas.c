41d064661f1165e79b1173376f6ea45573679633
Zsneik <joaquin.rivas.loncon@gmail.com>
2026-09-15 00:10:49 -0300

    Creando coligativas

--- quimica/coligativas.c
+++ quimica/coligativas.c
@@ -5,8 +5,12 @@ void menuColigativas(void)
 {
     char opcionMenu = 0;
     char iniciarPrograma = 1;
+    double factorVanthoff = 0;
+    double constante = 0;
+    double molalidad = 0;
+    double ebulloscopia = 0;
 
-    while(iniciarPrograma)
+while(iniciarPrograma)
     {
         printf("========== PROPIEDADES COLIGATIVAS ==========\n");
         printf("1: Elevacion ebullioscopica\n");
@@ -17,7 +21,13 @@ void menuColigativas(void)
 
     switch(opcionMenu)
         {
-            case 1:
+case 1:
+    printf("Ingrese la constante ebullioscopica (Kb): \n");
+    scanf("%lf", &constante);
+    printf("Ingrese el factor teorico de Vant Hoff\n");
+    scanf("%lf", &factorVanthoff);
+    printf("Ingrese la molalidad");
+    scanf("%lf", &molalidad);
                 break;
 
             case 0:
@@ -29,4 +39,14 @@ void menuColigativas(void)
                 break;
         }
     }
+}
+
+
+
+//=============EBULLOSCOPIA===========
+double calcularEbullioscopia(double factorVanthoff,
+                             double constante,
+                             double molalidad)
+{
+    return factorVanthoff * constante * molalidad;
 }
